3d58bb2f4e2f5d5e19378e8765f41b07840c4838
cristianlyle <161907552+cristianlyle@users.noreply.github.com>
2025-09-09 23:15:42 -0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,9 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "test";
+
+$servername = getenv('DB_HOST');
+$username   = getenv('DB_USER');
+$password   = getenv('DB_PASS');
+$dbname     = getenv('DB_NAME');
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
@@ -12,4 +13,10 @@ try {
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
+
+  //define {'DB_HOST','localhose'};
+  //define {'DB_NAME','test'};
+  //define {'DB_USER','root'};
+  //define {'DB_PASS',''};
+
 ?>
